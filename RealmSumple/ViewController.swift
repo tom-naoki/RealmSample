01b92d993219ce19d24b1f343e3a2159ebe524a1
倉富直城 <kuratomitnaoki@Naoki-noMacBook-Pro.local>
2018-07-16 19:44:16 +0900

    Initial Commit

---
+++ RealmSumple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmSumple
+//
+//  Created by 倉富直城 on 2018/07/16.
+//  Copyright © 2018年 com.kuratomi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
